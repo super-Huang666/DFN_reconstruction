30fa59f317fe9d72c7f4517f592262ebce298450
super-Huang666 <hc283416@163.com>
2026-03-11 16:27:43 +0800

    Draw logging line
    Draw logging line

--- onewell/displayLine.m
+++ onewell/displayLine.m
@@ -1,11 +1,11 @@
 function displayLine(linePoints)
     
-    % 提取测井线的起始点和终点坐标
+    % Extract the starting point and end point coordinates of the logging line
     startPoint = linePoints(1, :);
     endPoint = linePoints(2, :);
 
-    % 绘制测井线
-    hold on; % 保持当前图形，以便在上面添加新的图形元素
+    % Draw logging line
+    hold on; 
     plot3([startPoint(1), endPoint(1)], [startPoint(2), endPoint(2)], [startPoint(3), endPoint(3)], 'LineWidth', 2, 'Color', 'k');
-     hold off; % 恢复默认的绘图模式
+     hold off; 
 end
