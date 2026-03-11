e20cfb81ba79aeee9882b66df482dd811bb22e5d
super-Huang666 <hc283416@163.com>
2026-03-11 16:52:22 +0800

    Evaluate whether the DFN model meets the requirements
    Evaluate whether the DFN model meets the requirements

--- onewell/onewellevaluateSolution.m
+++ onewell/onewellevaluateSolution.m
@@ -1,13 +1,13 @@
-% 子函数：评估参数解
+% Subfunctions: evaluating parametric solutions
 function [objective, fnm, CFI_x, P10, selected] = onewellevaluateSolution(D, a, af, Nt, l_ratio, n, L, dim, CFI_x_initial, P10_initial,  bbx, pos, linePoints)
     lenPusPos = fracLenCen(l_ratio, D, n, a, af , L, Nt, dim);
     fnm = Field_DFN(DFN('dim', dim, 'n', Nt, 'shape', 'e', 'dip', 41, 'ddip', -1.97, 'dir', 167, 'ddir', -0.89, 'bbx', bbx, 'pos', pos, 'lenPusPos', lenPusPos), 'Poly');
     [CFI_x, P10, selected] = CFI(L, linePoints, fnm);
     
-    % 检查CFI长度是否匹配
+    % Check whether CFI length matches
     if numel(CFI_x) ~= numel(CFI_x_initial)
 %     if numel(P10) ~= numel(P10_initial)
-        objective = Inf; % 无效解
+        objective = Inf; % Invalid solution
         return;
     end
     
